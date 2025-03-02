0a4c3630fd18fb61e35079361557eace860c27de
Arcloan <95delchris@gmail.com>
2025-03-02 18:17:55 +0100

    Add some minor tweaks I forgot

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -5,7 +5,7 @@ import { Result } from "./Result";
 export function Container(){
     const [result, setResult] = useState(null);
     return (
-        <div className="grid mx-auto md:grid-cols-2 md:max-w-[80%] md:rounded-xl bg-white">
+        <div className="grid mx-auto md:grid-cols-2 md:max-w-[70%] md:rounded-xl bg-white">
             <Form setResultOnSubmit={setResult}></Form>
             <Result value={result}></Result>
         </div>
